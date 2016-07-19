f351fe90664be10c2072ba16b62feeb56b17504e
jcollins <jcollins@digitalglobe.com>
2016-07-19 17:29:04 -0600

    Added area based change detection workflow command line tool.

--- setup.py
+++ setup.py
@@ -2,14 +2,17 @@ from setuptools import setup
 
 setup(
     name='gbdxcli',
-    version='0.0.2',
-    py_modules=['gbdxcli'],
+    version='0.0.1',
     install_requires=[
         'Click',
-        'gbdxtools'
+        'gbdxtools',
+        'gbdx-auth'
     ],
     entry_points='''
         [console_scripts]
-        gbdx=gbdxcli:cli
-    '''
+        gbdx=gbdxcli.gbdxcli:cli
+    ''',
+    scripts=['bin/set_s3_creds',
+             ]
+
 )
